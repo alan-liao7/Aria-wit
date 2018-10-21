d1e078b372822e9b611735f82997ab912571e2f0
Kawser Nasim <kawsernasim@Kawsers-MacBook-Pro.local>
2018-10-20 23:05:52 -0400

    added code for bot

---
+++ api/routes/bot/bot.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const facebook = require('fb-messenger-bot-api');
+const messagingClient = new facebook.FacebookMessagingAPIClient(process.env.PAGE_ACCESS_TOKEN);
+const messageParser = facebook.FacebookMessageParser;
+
+router.get('/api/webhook',facebook.ValidateWebhook.validateServer);
+router.post('/api/webhook', (req, res) => {
+    const incomingMessages = messageParser.parsePayload(req.body);  
+
+    messagingClient.markSeen(senderId)
+        .then(() => client.toggleTyping(senderId,true))
+        .catch((err) => console.log(error));
+
+    //promise based reaction on message send confirmation
+    messagingClient.sendTextMessage(senderId, 'Hello')
+        .then((result) => console.log(`Result sent with: ${result}`));
+
+    //callback based reaction on message confirmation
+    messagingClient.sendTextMessage(senderId, 'Hello',(result) => console.log(`Result sent with: ${result}`));
+
+    //silent message sending
+    messagingClient.sendTextMessage(senderId,'Hello');
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -9,8 +9,11 @@ app.use(bodyParser.json());
 
 const phonePush = require('./api/routes/verification/push');
 const phoneVerification = require('./api/routes/verification/verify');
+const botAPI = require('./api/routes/bot/bot');
+
 
 app.use('/verification/push', phonePush);
 app.use('/verification/verify', phoneVerification);
+app.use('/bot', botAPI);
 
 module.exports = app;
